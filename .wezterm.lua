a7c47645e92d4877571b7a11585ac38b732da30e
songboyao <boyao.song@huawei.com>
2023-07-18 16:00:18 +0100

    Added wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.font = wezterm.font 'ComicCodeLigatures Nerd Font'
+config.default_domain = 'WSL:ubuntu2004'
+config.color_scheme = 'nord'
+config.keys = {
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+    {key="Backspace", mods="OPT", action=wezterm.action{SendString="\x1b\x7f"}},
+}
+config.window_close_confirmation = "NeverPrompt"
+config.font_size = 9
+config.hide_tab_bar_if_only_one_tab = true
+config.freetype_load_target = "HorizontalLcd"
+config.window_padding = {
+  left = '0px',
+  right = '0px',
+  top = '0px',
+  bottom = '0px',
+}
+return config
